0037c3a6ac7114376e9fa70458a8166d9deb9ede
你敢凝视深渊嘛 <53417969+17562105692@users.noreply.github.com>
2020-02-19 13:36:12 +0800

    add

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,10 +4,12 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    
   },
+  solarAndLunar(e){
+    this.setData({
+      alltime:e.detail
+    })
+  }
  
 })
